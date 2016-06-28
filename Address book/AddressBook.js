c9b93f58b9460569e9597c638ed08945e07e471f
Denis Volchkevich <den@volchkevich.pro>
2016-06-29 02:55:54 +0300

    Add address book first data

---
+++ Address book/AddressBook.js
@@ -0,0 +1,21 @@
+// User storage
+var users = [
+    {
+        firstName: 'Dmitry',
+        lastName: 'Bondarchuk',
+        birthDay: '16.07.1990',
+        phones: [
+            '89001234567',
+            '83431234567'
+        ]
+    },
+    {
+        firstName: 'Ivan',
+        lastName: 'Petrov',
+        birthDay: '08.06.1956',
+        phones: [
+            '89001234567',
+            '83431234567'
+        ]
+    }
+];
